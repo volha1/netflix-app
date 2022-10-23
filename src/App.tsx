024482a0554d415519f1cdc869eb7a93c195c4bd
volha1 <volha_skorkina@epam.com>
2022-10-23 14:15:16 +0200

    feat: change lint-staged config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import image from './react.png'
-import logo from './logo.svg'
+import image from './react.png';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div>
-      <h1>Netflix application</h1>
-      <img src={image} alt="React logo" width="300"></img>
-      <img src={logo} alt="React logo" width="100"></img>
+      <h1>Netflix Application</h1>
+      <img src={image} alt="React logo" width="300" />
+      <img src={logo} alt="React logo" width="100" />
     </div>
   );
 }
