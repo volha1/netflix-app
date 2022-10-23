8c0d49be7f431a226b9024042fb797e186bdf87a
volha1 <volha_skorkina@epam.com>
2022-10-23 13:02:10 +0200

    feat: add lint-staged and husky

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <h1>Netflix application</h1>;
+  return (
+    <div>
+      <h1>Netflix application</h1>
+    </div>
+  );
 }
 
 export default App;
